116c247d44b3d024b240001c1f94752a0db09f2b
Galymzhan Abdugalimov <abdu.galymzhan@gmail.com>
2017-12-07 20:43:08 +0600

    basic inline buttons

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
+import sys
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 import utils
 
+def my_excepthook(type, value, tb):
+    # you can log the exception to a file here
+    print('In My Exception Handler')
+
+    # the following line does the default (prints it to err)
+    sys.__excepthook__(type, value, tb)
+
+sys.excepthook = my_excepthook
+
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ParseMode
+from telegram import ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
 
 updater = Updater(token = os.environ.get("BOT_TOKEN"))
 
@@ -24,14 +34,26 @@ def start(bot, update):
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-
 def address(bot, update):
     address_text = update.message.text
     answer = "*Адрес:* %s\n*Почтовый индекс:* %s\n*Старый почтовый индекс:* %s" % utils.get_zip_code(address_text)
+
+    if update.callback_query:
+        print("ok")
+        bot.answer_callback_query(update.callback_query.id)
+
+    button_list = [
+        InlineKeyboardButton("«", callback_data="1"),
+        InlineKeyboardButton("»", callback_data="2")
+    ]
+
     
+    reply_markup = InlineKeyboardMarkup(utils.build_menu(button_list, n_cols=2))
+
     bot.send_message(chat_id=update.message.chat_id, 
                      text=answer, 
-                     parse_mode=ParseMode.MARKDOWN)
+                     parse_mode=ParseMode.MARKDOWN,
+                     reply_markup=reply_markup)
 
 address_handler = MessageHandler(Filters.text, address)
 dispatcher.add_handler(address_handler)
--- utils.py
+++ utils.py
@@ -10,3 +10,14 @@ def get_zip_code(address):
     oldCode = rJson['data'][0]['fullAddress']['oldPostcode']
     address_rus = rJson['data'][0]['addressRus']
     return (address_rus, code, oldCode)
+    
+def build_menu(buttons,
+               n_cols,
+               header_buttons=None,
+               footer_buttons=None):
+    menu = [buttons[i:i + n_cols] for i in range(0, len(buttons), n_cols)]
+    if header_buttons:
+        menu.insert(0, header_buttons)
+    if footer_buttons:
+        menu.append(footer_buttons)
+    return menu
